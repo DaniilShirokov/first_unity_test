726e9c057ce9fff85f0d6e655d04b9590015b19a
Damiil Shirokov <daniil196196@gmail.com>
2024-05-22 00:36:34 +0500

    fix commit

--- src/test/java/ru/netology/SimpleAccountTest.java
+++ src/test/java/ru/netology/SimpleAccountTest.java
@@ -1,6 +1,10 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SimpleAccountTest {
    @Test
@@ -12,7 +16,7 @@ class SimpleAccountTest {
                 3001
         };
         for (long amount : amountArray) {
-           // Assertions.assertTrue( SimpleAccount.add(amount));
+           Assertions.assertTrue( SimpleAccount.add(amount));
         }
     }
 
